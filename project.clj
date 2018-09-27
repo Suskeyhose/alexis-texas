4fe7e1b09b301e96f5682c32aadd936fd99fcf4d
Suskeyhose <k.chaldain@gmail.com>
2018-09-26 20:34:33 -0400

    Change to snapshot again

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject alexis-texas "0.1.1"
+(defproject alexis-texas "0.1.1-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
