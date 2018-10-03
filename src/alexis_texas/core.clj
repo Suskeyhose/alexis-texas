fa8772345ae343434faed5cd5f1d62bb3e4f6573
Suskeyhose <k.chaldain@gmail.com>
2018-10-03 17:29:19 -0400

    Add debugging vars

--- src/alexis_texas/core.clj
+++ src/alexis_texas/core.clj
@@ -191,6 +191,10 @@
   (swap! state assoc :running false)
   (spit "quotes.edn" (pr-str (:guilds @state))))
 
+(defonce ^:dynamic *events* (atom nil))
+(defonce ^:dynamic *connection* (atom nil))
+(defonce ^:dynamic *messaging* (atom nil))
+
 (defn -main
   "Starts the alexis-texas bot."
   []
@@ -199,10 +203,10 @@
                               (log/info e "No quotes file exists, starting with empty map.")
                               nil))
                        {})
-        events (a/chan 100)
-        connection (c/connect-bot! token events
-                                   :buffer-size 500000)
-        messaging (m/start-connection! token)]
+        events (reset! *events* (a/chan 100))
+        connection (reset! *connection* (c/connect-bot! token events
+                                                        :buffer-size 500000))
+        messaging (reset! *messaging* (m/start-connection! token))]
     (reset! state {:connection connection
                    :events events
                    :messaging messaging
