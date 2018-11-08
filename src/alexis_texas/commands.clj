e824949c9332542e38467f8153667ba90ec76988
Suskeyhose <k.chaldain@gmail.com>
2018-11-08 05:14:40 -0500

    Fix help message for admins

--- src/alexis_texas/commands.clj
+++ src/alexis_texas/commands.clj
@@ -16,7 +16,7 @@
   "Takes a core.async channel for communicating with the messaging process,
   the channel ID on which to send a help message, and the current guild's prefix,
   and sends a help message to Discord."
-  [messaging channel-id prefix]
+  [messaging channel-id prefix admin?]
   (m/send-message! messaging channel-id
                    (str "Hi! I'm Alexis Texas, a Discord Bot meant to help you keep"
                         " track of funny and interesting quotes in your server.\n\n"
@@ -31,10 +31,20 @@
                         "`" prefix "quote remove <name> <quote>` removes the given quote."
                         " Later this will be done by an ID instead of the copied and"
                         " pasted quote.\n\n"
-
-                        "If you are an admin on the server, feel free to make"
-                        " use of these additional commands:\n"
-                        "`" prefix "prefix <new prefix>` -- NOT YET IMPLEMENTED\n")))
+                        (when admin?
+                          (str "If you are an admin on the server, feel free to make"
+                               " use of these additional commands:\n"
+                               "`" prefix "prefix <new prefix>` changes the bot's prefix for this server.\n"
+                               "`" prefix "blacklist add <name>` bans members joining the server with this in"
+                               " their name.\n"
+                               "`" prefix "blacklist add regex <regex>` bans members joining the server with "
+                               "names that match the regular expression.\n"
+                               "`" prefix "blacklist remove <listed number>` removes the blacklist name or "
+                               "pattern based on its index as shown by the list command.\n"
+                               "`" prefix "blacklist list` lists all the names and patterns"
+                               " blacklisted by this bot.\n"
+                               "`" prefix "blacklist clear` clears the blacklist of both names and"
+                               " patterns.\n")))))
 
 (def owner (resource "owner.txt"))
 (def bot-id (resource "bot.txt"))
@@ -47,8 +57,11 @@
       {:keys [roles]} :member
       :keys [content channel-id guild-id mentions] :as event-data}]
   (when-not bot
-    (let [prefix (or (select-first [ATOM :state guild-id :prefix] state)
-                     "!")]
+    (let [prefix (or (select-first [ATOM :state (keypath guild-id) :prefix] state)
+                     "!")
+          admin? (or (= id owner)
+                     (= id (select-any [ATOM :guilds (keypath guild-id) :owner-id] state))
+                     (user-has-permission? id guild-id :manage-guild))]
       (commands prefix content
         (#"disconnect"
           (when (= id owner)
@@ -57,13 +70,13 @@
         (#"quote\s+add\s+(\S+)\s+([\s\S]+)" [user q]
           (m/send-message! (:messaging @state) channel-id
                            (str "Adding quote to user " user))
-          (transform [ATOM :state guild-id :quotes user] #(conj (or % []) q) state))
+          (transform [ATOM :state (keypath guild-id) :quotes (keypath user)] #(conj (or % []) q) state))
         (#"quote\s+remove\s+(\S+)\s+([\s\S]+)" [user q]
           (m/send-message! (:messaging @state) channel-id
                            (str "Removing quote from user " user))
-          (transform [ATOM :state guild-id :quotes user] #(filter (partial not= q) %) state))
+          (transform [ATOM :state (keypath guild-id) :quotes (keypath user)] #(filter (partial not= q) %) state))
         (#"quote\s+(\S+)" [user]
-          (let [quotes-vec (select [ATOM :state guild-id :quotes user ALL] state)]
+          (let [quotes-vec (select [ATOM :state (keypath guild-id) :quotes (keypath user) ALL] state)]
             (if-not (empty? quotes-vec)
               (m/send-message! (:messaging @state) channel-id
                                (str user ": " (rand-nth quotes-vec)))
@@ -71,7 +84,7 @@
                                (str "No quotes found for user " user "!")))))
         (#"quote\s*$"
           (let [quotes-vec (filter #(pos? (count (second %)))
-                                   (select [ATOM :state guild-id :quotes ALL] state))]
+                                   (select [ATOM :state (keypath guild-id) :quotes ALL] state))]
             (if-not (empty? quotes-vec)
               (let [[user quotes] (rand-nth quotes-vec)]
                 (m/send-message! (:messaging @state) channel-id
@@ -79,66 +92,56 @@
               (m/send-message! (:messaging @state) channel-id
                                "No quotes in this server! Get to talking!"))))
         (#"prefix\s+(\S+)" [new-prefix]
-          (if (or (= id owner)
-                  (= id (select-any [ATOM :guilds (keypath guild-id) :owner-id] state))
-                  (user-has-permission? id guild-id :manage-guild))
+          (if admin?
             (do (m/send-message! (:messaging @state) channel-id
                                  (str "Using new prefix: " new-prefix))
                 (setval [ATOM :state guild-id :prefix] new-prefix state))
             (m/send-message! (:messaging @state) channel-id
                              "You don't have permissions to change that!")))
         (#"blacklist\s+add\s+regex\s+([\S\s]+)" [blacklist-item]
-          (when (or (= id owner)
-                    (= id (select-any [ATOM :guilds (keypath guild-id) :owner-id] state))
-                    (user-has-permission? id guild-id :manage-guild))
-            (transform [ATOM :state guild-id :blacklist]
+          (when admin?
+            (transform [ATOM :state (keypath guild-id) :blacklist]
                        #(conj (or % []) (re-pattern blacklist-item))
                        state)
             (m/send-message! (:messaging @state) channel-id
                              "Adding new blacklisted pattern")))
         (#"blacklist\s+add\s+([\S\s]+)" [blacklist-item]
-          (when (or (= id owner)
-                    (= id (select-any [ATOM :guilds (keypath guild-id) :owner-id] state))
-                    (user-has-permission? id guild-id :manage-guild))
-            (transform [ATOM :state guild-id :blacklist]
+          (when admin?
+            (transform [ATOM :state (keypath guild-id) :blacklist]
                        #(conj (or % []) blacklist-item)
                        state)
             (m/send-message! (:messaging @state) channel-id
                              "Adding new blacklisted name")))
         (#"blacklist\s+list"
-          (when (or (= id owner)
-                    (= id (select-any [ATOM :guilds (keypath guild-id) :owner-id] state))
-                    (user-has-permission? id guild-id :manage-guild))
+          (when admin?
             (m/send-message! (:messaging @state) channel-id
                              (apply str "Blacklisted names:\n"
                                     (interpose
                                      "\n"
                                      (map-indexed
                                       #(str %1 ": " %2)
-                                      (select [ATOM :state guild-id :blacklist ALL]
+                                      (select [ATOM :state (keypath guild-id) :blacklist ALL]
                                               state)))))))
         (#"blacklist\s+clear"
-          (when (or (= id owner)
-                    )
+          (when admin?
             (setval [ATOM :state guild-id :blacklist ALL] NONE state)
             (m/send-message! (:messaging @state) channel-id
                              "Clearing the blacklist!")))
         (#"blacklist\s+remove\s+(\d+)" [idx]
-          (when (or (= id owner)
-                    (= id (select-any [ATOM :guilds (keypath guild-id) :owner-id] state))
-                    (user-has-permission? id guild-id :manage-guild))
+          (when admin?
             (let [num (Long/parseLong idx)
-                  item (select-first [ATOM :state guild-id :blacklist num] state)]
+                  item (select-first [ATOM :state (keypath guild-id) :blacklist (keypath num)] state)]
               (setval [ATOM :state guild-id :blacklist num] NONE state)
               (m/send-message! (:messaging @state) channel-id
                                (str "Removing blacklist item: "
                                     item)))))
         (#"help"
-          (display-help-message (:messaging @state) channel-id prefix))
+          (display-help-message (:messaging @state) channel-id prefix admin?))
         :default
         (when (and (= (count mentions) 1)
                    (= (:id (first mentions)) bot-id))
-          (display-help-message (:messaging @state) channel-id prefix))))))
+          (display-help-message (:messaging @state) channel-id prefix
+                                admin?))))))
 
 (defmethod handle-event :disconnect
   [event-type event-data]
