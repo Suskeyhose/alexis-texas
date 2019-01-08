c539b58372ccfbf7ef5ffcccb29d8258a82cd9a1
Suskeyhose <k.chaldain@gmail.com>
2019-01-08 13:35:51 -0500

    Update version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject alexis-texas "0.1.4-SNAPSHOT"
+(defproject alexis-texas "0.1.5-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
