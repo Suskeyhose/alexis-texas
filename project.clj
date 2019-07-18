9f1fa0650978cd9d5ab405e35448aadcf2539783
Suskeyhose <k.chaldain@gmail.com>
2019-07-17 21:50:24 -0500

    Version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject alexis-texas "0.2.3"
+(defproject alexis-texas "0.2.4"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
